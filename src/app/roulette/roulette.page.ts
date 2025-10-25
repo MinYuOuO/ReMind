62814903e253023217409d764781c7934f18c62b
KaiJie <Chow.Kaijie@raffles.university>
2025-10-25 15:10:34 +0800

    feat(roulette): enhance imports by adding navigation and button components

--- src/app/roulette/roulette.page.ts
+++ src/app/roulette/roulette.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonIcon, IonBackButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-roulette',
   templateUrl: 'roulette.page.html',
   styleUrls: ['roulette.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonButtons, IonButton, IonIcon, IonBackButton,
+    RouterModule,
+    ExploreContainerComponent
+  ],
 })
 export class RoulettePage {
   constructor() {}
